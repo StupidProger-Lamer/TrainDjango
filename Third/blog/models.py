5e9ba83a7dee1ce4bb4f8e9bd9db14404008d4ac
StupidProger-Lamer <grisga2001@gmail.com>
2020-01-26 14:51:00 +0700

    add models

--- Third/blog/models.py
+++ Third/blog/models.py
@@ -11,3 +11,43 @@ class Category(models.Model):
 	class Meta:
 		verbose_name = 'Категория'
 		verbose_name_plural = 'Категории'
+
+class Tag(models.Model):
+	'''Модель Тегов'''
+	name = models.CharField('Имя', max_length=100)
+	slug = models.SlugField('url', max_length=100)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Тег'
+		verbose_name_plural = 'Теги'
+
+class Post(models.Model):
+	'''Модель постов'''
+	title = models.CharField('Имя', max_length=100)
+	mini_text = models.TextField('Мини-текст', max_length=150)
+	text = models.TextField('Текст', max_length=2500)
+	published = models.DateTimeField('Дата публикации')
+	slug = models.SlugField('url', max_length=100)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		verbose_name = 'Пост'
+		verbose_name_plural = 'Посты'
+
+class Comment(models.Model):
+	'''Модель комментариев'''
+	text = models.TextField('Текст')
+	published = models.DateTimeField('Дата публикации')
+	moderation = models.BooleanField()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Комментарий'
+		verbose_name_plural = 'Комментарии'
--- Third/blog/views.py
+++ Third/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Category, Post
+
+class HomeView(View):
+	'''Home page'''
+	def get(self, request):
+		category_list = Category.objects.all()
+		posts = Post.objects.all()
+		return render(request, 'blog/home.html', {'categories': category_list, 'posts': posts})
+
+	def post(self, request):
+		pass
+
+class CategoryView(View):
+	'''Вывод статей категории'''
+	def get(self, request, slug):
+		category = Category.objects.get(slug=slug)
+		return render(request, 'blog/post_list.html', {'category': category})
