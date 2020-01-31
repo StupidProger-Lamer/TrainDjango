405843e1389bd4c1d08aa0f58ac5cbc0f15ad909
StupidProger-Lamer <grisga2001@gmail.com>
2020-01-31 13:25:49 +0700

    'update'

--- Third/blog/urls.py
+++ Third/blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.HomeView.as_view()),
-	path('<slug:cat_name>/', views.CategoryView.as_view(), name='category'),
-	path('<slug:category>/<slug:slug>/', views.PostDetailView.as_view(), name='detail_post')
+    path('tag/<slug:slug>/', views.PostListView.as_view(), name='tag'),
+    path("<slug:category>/<slug:slug>/", views.PostDetailView.as_view(), name="detail_post"),
+    path("<slug:category_slug>/", views.PostListView.as_view(), name="category"),
+    path("", views.PostListView.as_view()),
 ]
-
--- Third/blog/views.py
+++ Third/blog/views.py
@@ -1,29 +1,62 @@
-from django.shortcuts import render
-from django.views.generic.base import View
 from datetime import datetime
 
-from .models import Category, Post
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.base import View
 
-class HomeView(View):
-	'''Home page'''
-	def get(self, request):
-		category_list = Category.objects.all()
-		posts = Post.objects.filter(published=True, published_date__lte=datetime.now())
-		return render(request, 'blog/post_list.html', {'categories': category_list, 'posts': posts})
+from .models import Category, Post, Comment, Tag
+from .forms import CommentForm
 
-	def post(self, request):
-		pass
+
+class PostListView(View):
+    """Вывод статей категории"""
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=datetime.now(), published=True)
+
+    def get(self, request, category_slug=None, slug=None):
+        category_list = Category.objects.filter(published=True)
+        if category_slug is not None:
+            posts = self.get_queryset().filter(category__slug=category_slug,
+                                               category__published=True)
+        elif slug is not None:
+            posts = self.get_queryset().filter(tags__slug=slug, tags__published=True)
+        else:
+            posts = self.get_queryset()
+        if posts.exists():
+            template = posts.first().get_category_template()
+        else:
+            template = "blog/post_list.html"
+        return render(request, template, {"post_list": posts, "categories": category_list})
+
 
 class PostDetailView(View):
-	'''Вывод полной статьи'''
-	def get(self, request, category, slug):
-		category_list = Category.objects.all()
-		post = Post.objects.get(slug=slug)
-		tags = post.get_tags()
-		return render(request, 'blog/post_detail.html', {'categories': category_list, 'post': post})
+    """Вывод полной статьи"""
 
-class CategoryView(View):
-	'''Вывод статей категории'''
-	def get(self, request, cat_name):
-		category = Category.objects.get(slug=cat_name)
-		return render(request, 'blog/post_list.html', {'category': category})
+    def get(self, request, **kwargs):
+        category_list = Category.objects.filter(published=True)
+        post = get_object_or_404(Post, slug=kwargs.get("slug"))
+        form = CommentForm()
+        return render(request, post.template,
+                      {"categories": category_list, "post": post, "form": form}
+        )
+
+    def post(self, request, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.post = Post.objects.get(slug=kwargs.get("slug"))
+            form.author = request.user
+            form.save()
+        return redirect(request.path)
+
+
+# class CreateCommentView(View):
+#     def post(self, request, pk):
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             form.post_id = pk
+#             form.author = request.user
+#             form.save()
+#         return HttpResponse(status=201)
